ffd6e1d03c66ed0082cbcd5acb68d3963d71c213
rinkeigun <huiqun.lin@gmail.com>
2016-06-08 07:14:50 +0900

    add sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main()
+{
+	printf("sizeof types\n") ;
+	printf("sizeof bool = %d\n", sizeof ( bool ) );
+	printf("sizeof char = %d\n", sizeof ( char ) );
+	printf("sizeof unsigned char = %d\n", sizeof ( unsigned char ) );
+	printf("sizeof int = %d\n", sizeof ( int ) );
+	printf("sizeof short int = %d\n", sizeof ( short int ) );
+	printf("sizeof long int = %d\n", sizeof ( long int ) );
+	printf("sizeof float = %d\n", sizeof ( float ) );
+	printf("sizeof double = %d\n", sizeof ( double ) );
+	printf("sizeof struct = %d\n", sizeof ( struct ) );
+	printf("sizeof pointer int = %d\n", sizeof ( struct ) );
+
+}
