98211acecac0a0f9623015f38dd4de319a48f04b
rinkeigun <huiqun.lin@gmail.com>
2016-06-09 11:46:30 +0900

    first module

---
+++ first_module/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/camos/my_source/first_module/hello.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/camos/my_source/first_module/hello.ko /home/camos/my_source/first_module/hello.o /home/camos/my_source/first_module/hello.mod.o
---
+++ first_module/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/camos/my_source/first_module/hello.ko
+/home/camos/my_source/first_module/hello.o
