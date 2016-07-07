7aa744f701049a86cbff2ced27d0aac2b50ff989
rinkeigun <huiqun.lin@gmail.com>
2016-07-08 00:25:54 +0900

    bit 演算

--- c/bit.c
+++ c/bit.c
@@ -14,6 +14,8 @@ void main()
 	bool d = 3 ;
 	bool e = -2 ;
 
+	short int g, h, i ;
+
 	printf("a=%d, b=%d, c=%d, d=%d, e=%d\n", a, b, c, d, e ) ;
 	printf("size a=%d, b=%d, c=%d, d=%d, e=%d bool=%d, char=%d\n", 
 		sizeof(a), 
@@ -23,4 +25,18 @@ void main()
 		sizeof(e),
 		sizeof(bool), 
 		sizeof(char) ) ;
+
+  g = 11;
+  h = 14;
+
+  printf("%#x(%d) AND %#x(%d) = %#x(%d)\n", g,g, h,h, g & h, g&h);
+  printf("%#x(%d) OR  %#x(%d) = %#x(%d)\n", g,g, h,h, g | h,g|h);
+  printf("%#x(%d) XOR %#x(%d) = %#x(%d)\n", g,g, h,h, g ^ h,g^h);
+  printf("NOT %#x(%d) = %#x(%d)\n", g,g, ~g, ~g);
+for( i=0; i<4; i++)
+{
+  printf("%#x = %d << %d\n", 1<<i, 1, i);
+}
+  printf("%#x = %d >> %d\n", 7>>1, 7, 1);
+
 }
