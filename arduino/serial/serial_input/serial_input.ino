5078c47d70f7851ea39a2bdca7b1941977a0a84d
unknown <>
2016-08-02 22:24:22 +0900

    arduino timer2

--- arduino/serial/serial_input/serial_input.ino
+++ arduino/serial/serial_input/serial_input.ino
@@ -1,4 +1,4 @@
-//#include <MsTimer2.h>
+#include <MsTimer2.h>
 
 int len = 5 ;
 char incomingByte[6];   // for incoming serial data
@@ -25,6 +25,31 @@ void no_command()
 
 void disp()
 {
+    //while(1)
+  {
+  if (Serial.available() > 0) {
+    incomingByte[cnt] = Serial.read();
+    cnt++;
+  }
+  if( cnt >= len-1 )
+  {   
+    memcpy(disp_char, incomingByte, len ) ;
+    disp_char[4]='\0' ;
+
+    
+    //sprintf(disp_char, "%5s", incomingByte ) ;
+    if( incomingByte[0] == 'a' )
+    {
+     flg = 1 ;
+    }
+    if(incomingByte[0] == 'b')
+    {
+     flg = 0 ;
+    }
+    cnt = 0 ;
+  }
+  
+  MsTimer2::start();
     if( flg == 1 )
   {
      have_command(  ) ;
@@ -33,13 +58,16 @@ void disp()
   {
     no_command() ;
   }
+  }
 }
 
 void setup() {
   Serial.begin(9600);
-//  MsTimer2::set(500, disp);
+  MsTimer2::set(1000, disp);
+  MsTimer2::start();
 }
 void loop() {
+  
   while(1)
   {
   if (Serial.available() > 0) {
@@ -63,14 +91,18 @@ void loop() {
     }
     cnt = 0 ;
   }
+  } 
 
+/*
   delay(100) ;
   if( cnt_delay++ >= 10 )
   {
     disp() ;
     cnt_delay = 0 ;
   }
-  // MsTimer2::start();
+  */
+   //MsTimer2::start();
+  //disp() ;
 
   // say what you got:
   /*
@@ -83,7 +115,9 @@ void loop() {
     no_command() ;
   }
 */
-  }
+/*  }
+
+ */
   
 }
 
