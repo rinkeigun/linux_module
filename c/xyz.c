9806567c3e7908dfd8ea224d8e30160d3d8f8d50
Huiqun Lin <huiqun.lin@gmail.com>
2017-05-06 23:20:38 +0900

    xyz Dockerfile

--- c/xyz.c
+++ c/xyz.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define INT int
+#define INT long long unsigned
 #define MAX( a, b )  (a)>(b)?(a):(b)
 #define SQRT2( a, b ) ((INT)sqrt( (a)*(a)+(b)*(b) ))
 #define SQRT3( a, b, c ) ((INT)sqrt( (a)*(a)+(b)*(b)+(c)*(c) ))
@@ -32,7 +32,7 @@ bool call2( INT a, INT b, INT *d )
 	//for( INT cnt = MAX(a, b); cnt < a+b; cnt ++)
 	for( INT cnt = s; cnt < s+2; cnt ++)
 	{
-		//printf("\t cnt=%d\n", cnt) ;
+		//printf("\t cnt=%llu\n", cnt) ;
 		if( (cnt * cnt) == ( a * a + b * b ) )
 		{
 			*d = cnt ;
@@ -54,26 +54,32 @@ int main(int argc, char *argv[])
 	int cnt4 = 0;
 
 	x = atol( argv[1] ) ;
-	//printf( "x = %d\n", x ) ;
-	//printf( "int 0.4, 0.5 = %d, %d\n", (INT)0.4, (INT)0.5 ) ;
+	//printf( "x = %llu\n", x ) ;
+	//printf( "int 0.4, 0.5 = %llu, %llu\n", (INT)0.4, (INT)0.5 ) ;
 
 	while(1)
 	//while(x<6)
 	{
 		for( y = 2; y < x ; y ++)
 		{
+				xy_flg = call2( x, y , &xy) ;
+				if( !xy_flg ) continue ;
+				//else printf("xy=%llu, ", xy ) ;
 			for( z = 1; z < y ; z ++)
 			{
-				//printf("1. x=%d, y=%d, z=%d\n", x, y, z);
+				//printf("\n x=%llu, y=%llu, z=%llu, ", x, y, z);
 				
-				xy_flg = call2( x, y , &xy) ;
 				yz_flg = call2( y, z , &yz) ;
+				if( !yz_flg ) continue ;
+				//else printf("yz=%llu, ", yz ) ;
 				zx_flg = call2( z, x , &zx) ;
+				if( !zx_flg ) continue ;
+				//else printf("zx=%llu, ", zx ) ;
 				xyz_flg = call3( x, y, z , &xyz) ;
 				
 				if( xy_flg && yz_flg && zx_flg && xyz_flg ) 
 				{
-					printf("x=%d, y=%d, z=%d, xy=%d, yz=%d, zx=%d, xyz=%d\n",
+					printf("x=%llu, y=%llu, z=%llu, xy=%llu, yz=%llu, zx=%llu, xyz=%llu\n",
 					x, y, z,
 					xy_flg? xy:0,
 					yz_flg? yz:0,
@@ -86,7 +92,7 @@ int main(int argc, char *argv[])
 					(zx_flg? 1:0)+
 					(xyz_flg ? 1:0)  ;
 				if( cnt4 >2 )
-				printf("x=%d, y=%d, z=%d, \x1b[46mxy=%d, yz=%d, zx=%d, xyz=%d\x1b[0m\n",
+				printf("x=%llu, y=%llu, z=%llu, \x1b[46mxy=%llu, yz=%llu, zx=%llu, xyz=%llu\x1b[0m\n",
 					x, y, z,
 					xy_flg? xy:0,
 					yz_flg? yz:0,
@@ -94,7 +100,7 @@ int main(int argc, char *argv[])
 					xyz_flg ? xyz:0 ) ;
 			}
 		}
-		printf("x=%d\n", x) ;
+		if( x%100 == 0 ) printf("x=%llu\n", x) ;
 		x ++ ;
 	}
 	return true;
