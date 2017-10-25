8eb1179f8db5648836b1c0fd201306d04901afc1
rinkeigun <huiqun.lin@gmail.com>
2017-10-25 14:54:58 +0000

    get module info

---
+++ python_source/moduledir.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#  file name : 
+#  author : 
+#  date :
+#  python3 -c "help('modules')"
+
+import math
+import sys
+print( math )
+print( sys.builtin_module_names )
+print( sys.modules.keys() )
