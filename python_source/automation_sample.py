ec7d7b32121ac8b32919dbf8d2b7d842eb21af7a
rinkeigun <huiqun.lin@gmail.com>
2018-03-14 04:33:21 +0900

    自動化ソフト、メモ帳の操作

---
+++ python_source/automation_sample.py
@@ -0,0 +1,17 @@
+from pywinauto import application
+import time
+
+app = application.Application()
+app.start("notepad.exe")
+app.Notepad.wait('visible')
+#time.sleep(3)
+app.Notepad.edit.TypeKeys('Hello world')
+app.Notepad.MenuSelect(u'ファイル(&F)->名前を付けて保存(&A)...')
+
+# 動作を分けてはだめ？
+#app.Notepad.MenuSelect(u'ファイル(&F)')[u'名前を付けて保存(&A)...']
+#app.Notepad.MenuSelect(u'ファイル(&F)')
+#app.Notepad.MenuSelect(u'名前を付けて保存(&A)...')
+
+app[u'名前を付けて保存'].edit.SetText('tamesi.txt')
+app[u'名前を付けて保存'][u'保存(&S)'].Click()
