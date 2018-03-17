76ebd7801be4f369dbe9ca1ab64a55cc25361a73
rinkeigun <huiqun.lin@gmail.com>
2018-03-17 13:44:15 +0900

    画質の向上

--- python_source/app_record.py
+++ python_source/app_record.py
@@ -11,9 +11,6 @@ import time
 import numpy
 import threading
 
-
-stoper = False
-
 class TestThread(threading.Thread):
 
     """docstring for TestThread"""
@@ -25,20 +22,20 @@ class TestThread(threading.Thread):
     def run(self):
         #時間を取得
         d=datetime.datetime.now().isoformat()
-        filerename=str(d)+'.avi'
+        filerename=(str(d)+'.avi').replace(":", "")
 
-        cameraCapture = cv2.VideoCapture(0)
-        fps = 30
-        size = (int(cameraCapture.get(3)),
-            int(cameraCapture.get(4)))
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        #ファイル名
         filename = "output5.avi"
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        
+        # 速度
+        fps = 100
+        
+        #サイズが小さく設定すると画質が落ちる
+        size = (1280,960)
         videoWriter = cv2.VideoWriter(filename, fourcc, fps, size)
 
-
-        #start=time()
-        while (cameraCapture.isOpened()):
-            ret, frame = cameraCapture.read()
+        while (1):
             img = self.app.CaptureAsImage()
             frame = numpy.asarray(img)
 
@@ -46,7 +43,7 @@ class TestThread(threading.Thread):
             cv2.imshow('frame', frame)
  
             # 圧縮
-            frame = cv2.resize(frame, (640, 480))
+            frame = cv2.resize(frame, size)
  
             # 書き込み
             videoWriter.write(frame)
@@ -55,23 +52,53 @@ class TestThread(threading.Thread):
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-        videowriter.close()
+        videoWriter.release()
         os.rename(filename,'%s'%(filerename))
         # 後処理
-        cameraCapture.release()
-        videoWriter.release()
         cv2.destroyAllWindows()
 
+#コーデック名	引数	拡張子
+#MP4S	('M', 'P', '4', 'S')	.mp4
+#MP4V	('M', 'P', '4', 'V')	.mp4
+#DIV3	('D', 'I', 'V', '3')	.avi
+#DIVX	('D', 'I', 'V', 'X')	.avi
+#I420	('I', '4', '2', '0')	.avi
+#IYUV	('I', 'Y', 'U', 'V')	.avi
+#MJPG	('M', 'J', 'P', 'G')	.avi
+#PIM1	('P', 'I', 'M', '1')	.avi
+#XVID	('X', 'V', 'I', 'D')	.avi
+#dv25	('d', 'v', '2', '5')	.wmv
+#dv50	('d', 'v', '5', '0')	.wmv
+#dvc	('d', 'v', 'c', ' ')	.wmv
+#dvh1	('d', 'v', 'h', '1')	.wmv
+#dvhd	('d', 'v', 'h', 'd')	.wmv
+#dvsd	('d', 'v', 's', 'd')	.wmv
+#dvsl	('d', 'v', 's', 'l')	.wmv
+#H263	('H', '2', '6', '3')	.wmv
+#M4S2	('M', '4', 'S', '2')	.wmv
+#MP43	('M', 'P', '4', '3')	.wmv
+#MPG1	('M', 'P', 'G', '1')	.wmv
+#MSS1	('M', 'S', 'S', '1')	.wmv
+#MSS2	('M', 'S', 'S', '2')	.wmv
+#WMV1	('W', 'M', 'V', '1')	.wmv
+#WMV2	('W', 'M', 'V', '2')	.wmv
+#WMV3	('W', 'M', 'V', '3')	.wmv
+#WVC1	('W', 'V', 'C', '1')	.wmv
+#mp4v	('m', 'p', '4', 'v')	.mov
 
 if __name__ == '__main__':
 
     time.sleep(1)
+    app_path = "notepad.exe"
     app = application.Application()
-    app.start("notepad.exe")
-    app.Notepad.wait('visible')
+    app.start(app_path)
+    print(app.Notepad.wait('visible'))
+    
+    spec_app = app.top_window()
 #    app[u'無題.*メモ帳'].CaptureAsImage().save('auto_capture.png')
     tmpapp = app[u'無題.*メモ帳']
-    th_cl = TestThread(tmpapp)
+    th_cl = TestThread(spec_app)
+#    th_cl = TestThread(spec_app)
     th_cl.start()
 
     #cap_app = AppCap(tmpapp)
