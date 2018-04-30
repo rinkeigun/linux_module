fd0784d6cde07d7bb18d5b19c93aaec3f9226b22
rinkeigun <huiqun.lin@gmail.com>
2018-05-01 01:55:33 +0900

    システム開始、システム終了、アプリ開始、アプリ終了 gpedit.mscでポリシーの変更、python によるレジスターの操作の追加調査が必要

--- python_source/windows_log_control.py
+++ python_source/windows_log_control.py
@@ -5,18 +5,43 @@ import win32evtlog
 import win32evtlogutil
 import winerror
 
-#logType = "System"
-logType = "Application"
+logType = "System"
+#logType = "Application"
+#logType = "Security"
+
 h = win32evtlog.OpenEventLog(None, logType)
 
 flags = win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
 objects = win32evtlog.ReadEventLog(h,flags, 0)
+
 while objects:
   for ev_obj in objects:
-    #appname, eventid, vnetcategory, eventtype, strings = win32evtlogutil.SafeFormatMessage(ev_obj, logType)
-    appname = win32evtlogutil.FormatMessage(ev_obj, logType)
-    print( type(appname))
-    print( appname )
+    try:
+      #if "WINWORD.EXE" in ev_obj.StringInserts:
+      if True:
+        print( 
+          #ev_obj.RecordNumber,
+          #ev_obj.TimeGenerated,
+          #ev_obj.TimeWritten,
+          ev_obj.EventID,
+          ev_obj.EventType, 
+          #ev_obj.EventCategory, 
+          #ev_obj.ReservedFlags, 
+          #ev_obj.ClosingRecordNumber, 
+          #ev_obj.SourceName,
+          ev_obj.StringInserts, 
+          #ev_obj.Sid, 
+          #ev_obj.Data, 
+          ev_obj.ComputerName )
+        print( win32evtlogutil.SafeFormatMessage(ev_obj, logType))
+      #appname, eventid, vnetcategory, eventtype, strings = win32evtlogutil.SafeFormatMessage(ev_obj, logType)
+      #appname = win32evtlogutil.FormatMessage(ev_obj, logType)
+      #print( type(appname))
+      #print( appname )
+    except :
+    	pass
+    finally:
+      pass
     '''
     if winerror.HRESULT_CODE(ev_obj.EventID) == 6005:
       print ("%s %s" % ("start", ev_obj.TimeGenerated.Format()))
