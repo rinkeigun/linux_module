b25e864415c2b6449e86f23f62b8f058242b145e
rinkeigun <huiqun.lin@gmail.com>
2018-06-22 10:52:53 +0900

    注釈を追加

--- python_source/pyocr_cv.py
+++ python_source/pyocr_cv.py
@@ -6,23 +6,45 @@ import pyocr.builders
 import numpy as np
 import cv2
 
+
+'''
+pagesegmode values are:
+0 = Orientation and script detection (OSD) only.
+1 = Automatic page segmentation with OSD.
+2 = Automatic page segmentation, but no OSD, or OCR
+3 = Fully automatic page segmentation, but no OSD. (Default)
+4 = Assume a single column of text of variable sizes.
+5 = Assume a single uniform block of vertically aligned text.
+6 = Assume a single uniform block of text.
+7 = Treat the image as a single text line.
+8 = Treat the image as a single word.
+9 = Treat the image as a single word in a circle.
+10 = Treat the image as a single character.
+'''
+
+
+
+filename = sys.argv[1]
+print( filename )
+
 tools = pyocr.get_available_tools()
 if len(tools) == 0:
 	print("No OCR tool found")
 	sys.exit(1)
 tool = tools[0]
 #a = pyocr.builders.TextBuilder(tesseract_layout=4)
-#a = pyocr.builders.WordBoxBuilder( )
-a = pyocr.builders.LineBoxBuilder( )
+a = pyocr.builders.WordBoxBuilder(tesseract_layout=6 )
+#a = pyocr.builders.LineBoxBuilder(tesseract_layout=6)
 
 txt = tool.image_to_string(
-	Image.open("a.png"),
+	Image.open(filename),
 	lang="jpn+eng",
-	builder  = a
+	builder = a
 )
-#print (txt)
+print (txt)
 
-out = cv2.imread("a.png")
+
+out = cv2.imread(filename)
 for d in txt:
 	print (d.content)
 	print (d.position)
@@ -31,3 +53,4 @@ for d in txt:
 cv2.imshow('image', out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
